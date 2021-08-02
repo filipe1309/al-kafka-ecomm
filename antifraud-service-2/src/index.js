b09e69cafce44d26ec26b36be24269228161e744
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-02 17:34:41 -0300

    feat: add antifrause service 2

---
+++ antifraud-service-2/src/index.js
@@ -0,0 +1,42 @@
+import { Kafka } from "kafkajs"
+
+/**
+ * Kafka connection
+ */
+const kafka = new Kafka({
+    clientId: "antifraud",
+    brokers: ["kafka:9094"],
+})
+
+const topic = "ECOMMERCE_NEW_ORDER"
+const consumer = kafka.consumer({ groupId: "antifraud-group" })
+
+// const producer = kafka.producer()
+
+async function run() {
+    // await producer.connect()
+    await consumer.connect()
+    await consumer.subscribe({ topic })
+    await consumer.run({
+        eachMessage: async({ topic, partition, message }) => {
+            const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+            console.log(`- ${prefix} ${message.key}#${message.value}`)
+
+            await new Promise(resolve => setTimeout(resolve, 5000));
+
+            console.log('Antifraud checked');
+            // const payload = JSON.parse(message.value)
+
+            // setTimeout(() => {
+            // producer.send({
+            //         topic: "CERTIFICATION_RESPONSE",
+            //         messages: [{
+            //             value: `Certificate of ${payload.user.name}, course ${payload.course}, generated!`,
+            //         }, ],
+            //     })
+            // }, 3000)
+        },
+    })
+}
+
+run().catch(console.error)
